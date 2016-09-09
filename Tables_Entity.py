57ee1e8fcbf3735b6af42d6d7757a35da57522cd
chitrakakkar <India@123>
2016-09-09 13:08:14 -0500

    Fixed main.
    Add Employee is working.
    Delete Employee is working.
    # Work on update info for an employee

--- Tables_Entity.py
+++ Tables_Entity.py
@@ -9,27 +9,30 @@ class Employee:
         self.grade = grade
 
 
-class PayScale(Employee):
-        def __init__(self, employee_id, first_name, last_name, grade):
-            Employee.__init__(self, employee_id, first_name, last_name, grade)  # Salary class inheriting from Employees
+class PayScale:
+    # def __init__(self, employee_id, first_name, last_name, grade):
+    #     Employee.__init__(self, employee_id, first_name, last_name, grade)  # Salary class inheriting from Employees
 
-            if self.grade == 1:
-                self.salary_scale = 35.67
+    def __init__(self, grade, salary):
+        self.grade = grade
+        self.salary = salary
+        if self.grade == 1:
+            self.salary = 35.67
 
-            if self.grade == 2:
-                self.salary_scale = 29.23
+        if self.grade == 2:
+            self.salary = 29.23
 
-            if self.grade == 3:
-                self.salary_scale = 25.07
+        if self.grade == 3:
+            self.salary = 25.07
 
-            if self.grade == 4:
-                self.salary_scale = 22.13
+        if self.grade == 4:
+            self.salary = 22.13
 
-            if self.grade == 5:
-                self.salary_scale = 20.17
+        if self.grade == 5:
+            self.salary = 20.17
 
-            if self.grade == 6:
-                self.salary_scale = 18.00
+        if self.grade == 6:
+            self.salary = 18.00
 
-            if self.grade == 7:
-                self.salary_scale = 15.00
+        if self.grade == 7:
+            self.salary = 15.00
--- main.py
+++ main.py
@@ -3,21 +3,26 @@ from Database_Manager import dataBase_manaGer
 db = dataBase_manaGer('Employee.db')
 
 
-def HR_Options(employee):
-    menu = ('\t1)Add Employee\n'
-            '\t2)Delete Employee\n'
-            '\t3)Update Employee\n'
-            '\t4)Quit\n')
+def HR_Options(employee, payScale):
+    menu = (
+        '\nOptions\n'
+        'Employee:{} {} has grade = {} which gives {} per hours\n'
+        '\t1)Add Employee\n'
+        '\t2)Delete Employee\n'
+        '\t3)Update Employee\n'
+        '\t4)Quit\n'
+        'Choose from above mentioned options'
+    ).format(employee.first_name, employee.last_name, employee.grade, payScale.salary)
 
     while True:
-        Option_choice = get_employee_int(menu, range(1, 4))
+        Option_choice = get_employee_int(menu, None)
 
         if Option_choice == 1:
             add_employee(employee)
         elif Option_choice == 2:
             delete_employee(employee)
         elif Option_choice == 3:
-            update_employee
+            update_employee(employee)
         else:
             exit(0);
 
@@ -26,7 +31,7 @@ def add_employee(employee):
     first_name = get_user_string("Enter your first name")
     last_name = get_user_string("Enter your last name")
     grade = get_employee_int("Enter your grade", range(1,7))
-    db.add_employee(first_name,last_name,grade)
+    db.add_employee(first_name, last_name, grade)
     print("New employee " + first_name + last_name + " has been added to the employee table")
 
 
@@ -35,10 +40,11 @@ def delete_employee(employee):
         delete_id = get_user_string("Enter the employee id to be delete")
         db.delete_employee(delete_id)
         print("Employee " + delete_id + " has been delete from employee")
+        break
 
 
 def update_employee(employee):
-    employee_id = get_employee_int("Enter the employee id you want to update",range(1, 10))
+    employee_id = get_employee_int("Enter the employee id you want to update", range(1, 10))
     newGrade = get_employee_int("Enter the new grade for " + employee_id, range(1, 7))
     db.update_employee(employee_id, newGrade)
     print(employee_id + " grade value has been update to " + newGrade)
@@ -61,7 +67,7 @@ def get_user_string(message):
             print('You must enter something.')
 
 
-def get_employee_int(message,valid_range=None):
+def get_employee_int(message, valid_range=None):
     while True:
         user_input = input('{}: '.format(message))
 
@@ -71,30 +77,24 @@ def get_employee_int(message,valid_range=None):
         except ValueError:
             print('You must enter a whole number.')
             continue
+
         #Range Validation
         if valid_range and number not in valid_range:
             _min = min(valid_range)
             _max = max(valid_range)
             print('You must enter a number from {} to {}.'.format(_min, _max))
             continue
-    return number
+        return number
 
 
 def main():
-    employee = None
-
-    while True:
-        employee_id = get_employee_int('Enter employee ID')
-        employee = db.get_employee(employee_id)
-        if not employee:
-            print("No employee found with id ", employee_id)
-            continue
-        break
-
-    if employee:
-        HR_Options(employee)
-
-        print("GoodBye!")
+    employee_id = get_employee_int('Enter employee ID ')
+    employee = db.get_employee(employee_id)
+    payscale = db.get_payScale(employee.grade)
+    if not employee:
+        print("No employee found with id ", employee_id)
+    else:
+        HR_Options(employee, payscale)
 
 main()
 
